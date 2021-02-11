4e1ecf20b2eeb3af2167bc27049851af8c643be1
Jacob Bare <jacob@parameter1.com>
2021-02-11 16:32:31 -0600

    Add express json error handlers

--- api/src/server.js
+++ api/src/server.js
@@ -1,6 +1,8 @@
+const createError = require('http-errors');
 const http = require('http');
 const express = require('express');
 const helmet = require('helmet');
+const { isDevelopment } = require('envalid');
 
 const service = express();
 service.set('trust proxy', ['loopback', 'linklocal', 'uniquelocal']);
@@ -8,6 +10,20 @@ service.set('trust proxy', ['loopback', 'linklocal', 'uniquelocal']);
 service.use(helmet());
 service.get('/', (req, res) => {
   res.json({ ping: 'pong' });
+});
+
+// Force Express to throw an error on 404s.
+service.use((req, res, next) => { // eslint-disable-line no-unused-vars
+  throw createError(404, `No route found for '${req.path}'`);
+});
+
+// eslint-disable-next-line no-unused-vars
+service.use((err, req, res, next) => {
+  const status = err.statusCode || err.status || 500;
+  const { message, stack } = err;
+  const obj = { error: true, status, message };
+  if (isDevelopment && stack) obj.stack = stack.split('\n');
+  res.status(status).json(obj);
 });
 
 module.exports = http.createServer(service);
