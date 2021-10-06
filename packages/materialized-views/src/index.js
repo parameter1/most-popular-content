b0668e14d03d539b2b0780e59747e4e76a5f57e0
Jacob Bare <jacob@parameter1.com>
2021-10-06 13:06:06 -0500

    Create root package exports

---
+++ packages/materialized-views/src/index.js
@@ -0,0 +1,3 @@
+export { filterMongoUri } from '@parameter1/events-repositories';
+export { default as weeklyContent } from './weekly-content.js';
+export { default as mongodbClient } from './mongodb/client.js';
