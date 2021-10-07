1c499a6faf80d32491837b3c8391b651b39957c5
Jacob Bare <jacob@parameter1.com>
2021-10-07 15:06:38 -0500

    Base tenants off of tenant list package

--- packages/materialized-views/src/utils/get-all-tenants.js
+++ packages/materialized-views/src/utils/get-all-tenants.js
@@ -1,13 +1,3 @@
-import mongodb from '../mongodb/client.js';
+import tenantList from '@parameter1/events-tenants';
 
-const pattern = /^p1-events-([a-z0-9]+)$/;
-
-export default async () => {
-  const { databases } = await mongodb.listDatabases();
-  return databases
-    .filter(({ name }) => pattern.test(name))
-    .map(({ name }) => {
-      const matches = pattern.exec(name);
-      return matches[1];
-    }).sort();
-};
+export default [...tenantList];
