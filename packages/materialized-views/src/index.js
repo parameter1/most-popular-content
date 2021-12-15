867c96b9203b7f5247a69fbf50836697215e9dbc
Shinsina <shinsina@att.net>
2021-12-15 14:04:34 -0600

    Add monthly-content aggregation to index.

--- packages/materialized-views/src/index.js
+++ packages/materialized-views/src/index.js
@@ -1,3 +1,4 @@
 export { filterMongoUri } from '@parameter1/events-repositories';
 export { default as weeklyContent } from './weekly-content.js';
+export { default as monthlyContent } from './monthly-content.js';
 export { default as mongodbClient } from './mongodb/client.js';
