b3d920ea4826ed4cef8e5c80e945195fe7248fd0
Shinsina <shinsina@att.net>
2021-12-15 14:50:49 -0600

    Condense lambda functions down to generate-content

--- packages/lambda/functions/generate-content/index.js
+++ packages/lambda/functions/generate-content/index.js
@@ -1,4 +1,4 @@
-import { monthlyContent, mongodbClient as mongodb, filterMongoUri } from '@most-popular-content/materialized-views';
+import { generateContent, mongodbClient as mongodb, filterMongoUri } from '@most-popular-content/materialized-views';
 
 const { log } = console;
 
@@ -9,7 +9,10 @@ export default ({ AWS_EXECUTION_ENV }) => async (_, context = {}) => {
   const conn = await mongodb.connect();
   log(`MongoDB connected to ${filterMongoUri(conn)}`);
 
-  await monthlyContent();
+  await Promise.all([
+    generateContent({ granularity: 'week' }),
+    generateContent({ granularity: 'month' }),
+  ]);
 
   if (!AWS_EXECUTION_ENV) await mongodb.close();
 };
