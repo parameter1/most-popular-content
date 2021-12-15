47b3eeec357822cef95f44bb1c80b588d0308b80
Shinsina <shinsina@att.net>
2021-12-15 14:05:34 -0600

    Add monthly-content lambda function.

---
+++ packages/lambda/functions/monthly-content/index.js
@@ -0,0 +1,15 @@
+import { monthlyContent, mongodbClient as mongodb, filterMongoUri } from '@most-popular-content/materialized-views';
+
+const { log } = console;
+
+export default ({ AWS_EXECUTION_ENV }) => async (_, context = {}) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  log('Connecting to MongoDB...');
+  const conn = await mongodb.connect();
+  log(`MongoDB connected to ${filterMongoUri(conn)}`);
+
+  await monthlyContent();
+
+  if (!AWS_EXECUTION_ENV) await mongodb.close();
+};
