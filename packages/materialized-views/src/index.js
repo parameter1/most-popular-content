091dbdc78a0ff36f34dfc3ec2d9aa6f12a8f363a
Jacob Bare <jacob@parameter1.com>
2022-03-22 08:54:37 -0500

    Create initial hourly content generator

--- packages/materialized-views/src/index.js
+++ packages/materialized-views/src/index.js
@@ -1,3 +1,4 @@
 export { filterMongoUri } from '@parameter1/events-repositories';
 export { default as generateContent } from './generate/content.js';
+export { default as generateContentHourly } from './generate/content-hourly.js';
 export { default as mongodbClient } from './mongodb/client.js';
