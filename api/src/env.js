2c9149dfcff8cab6be3f02830560547d1c7182cb
Jacob Bare <jacob@parameter1.com>
2022-03-22 11:53:41 -0500

    Add redis docker service and client

--- api/src/env.js
+++ api/src/env.js
@@ -11,10 +11,12 @@ export const {
   NEW_RELIC_ENABLED,
   NEW_RELIC_LICENSE_KEY,
   PORT,
+  REDIS_DSN,
 } = cleanEnv(process.env, {
   HOST: str({ desc: 'The host that the service will run on.', default: '0.0.0.0' }),
   MONGO_URL: str({ desc: 'The P1 Events MongoDB instance to connect to.' }),
   NEW_RELIC_ENABLED: bool({ desc: 'Whether New Relic is enabled.', default: true, devDefault: false }),
   NEW_RELIC_LICENSE_KEY: str({ desc: 'The license key for New Relic.', devDefault: '(unset)' }),
   PORT: port({ desc: 'The port that the service will run on.', default: 80 }),
+  REDIS_DSN: str({ desc: 'The Redis cache DSN to connect to.' }),
 });
