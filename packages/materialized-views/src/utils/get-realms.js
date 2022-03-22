f30f9a4fde2f0a475293413bcb27c232e6597905
Jacob Bare <jacob@parameter1.com>
2022-03-22 10:48:55 -0500

    Ensure realm matches site ID

--- packages/materialized-views/src/utils/get-realms.js
+++ packages/materialized-views/src/utils/get-realms.js
@@ -1,4 +1,7 @@
-export default async (repos) => {
-  const values = await repos.eventObject.distinct({ key: 'realm' });
+export default async (eventTenantRepos) => {
+  const values = await eventTenantRepos.eventObject.distinct({
+    key: 'realm',
+    query: { realm: /^[a-f0-9]{24}$/ },
+  });
   return values.filter((v) => v).map((v) => v.trim()).filter((v) => v);
 };
